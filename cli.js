673a4591a1eb45ba26cb789f26bedc4d283a1909
shubh <shubhamdixit863@gmail.com>
2023-07-01 21:02:13 +0530

    packages

---
+++ cli.js
@@ -0,0 +1,17 @@
+console.log(Sum(process.argv[2],process.argv[3]));
+
+
+// cli app to add two numbers in node js 
+
+function Sum(a,b){
+    return Number(a)+Number(b);
+
+}
+
+
+
+
+// --You have to create a command line dictionary --
+// where user will enter the word and you will have to outpu them its definition
+
+// [{"Cat":"A feline animal"}]
---
+++ globals.js
@@ -0,0 +1,10 @@
+// global variables in node js
+
+console.log(JSON.stringify(Object.getOwnPropertyNames(globalThis)));
+
+// fs module -->file system module 
+// events module in node 
+// stream module in node js 
+// http module in node js
+//path module
+// os module
