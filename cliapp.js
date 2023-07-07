2234b96e1d3e491394ea863229e394a2e8cfe171
shubh <shubhamdixit863@gmail.com>
2023-07-07 22:24:56 +0530

    cli app

--- cliapp.js
+++ cliapp.js
@@ -1,5 +1,8 @@
-const readline=require("readline");
+const readline=require("readline"); //
 const {stdin:input,stdout:output}=require("process");
+var Spinner = require('cli-spinner').Spinner;
+
+
 function Input(){
 // We will be creating an interface by comnbining both input and output
 const rl=readline.createInterface({input,output});
@@ -11,6 +14,33 @@ rl.question("What is your name?? \n",(answer)=>{
 })
 }
 
+// solid 
+// separation of concern -->single responsibility pro
+function CardDetails(cs){
+    cs.question("Please enter your 10 digit card details \n",(card)=>{
+        if (card.length!=10){
+            CardDetails(cs)
+           // console.log("Invalid card details");
+           // cs.close();
+            return ;
+        }
+        // We will print out the loader --->
+        var spinner = new Spinner('processing.. %s\n');
+        spinner.setSpinnerString('|/-\\');
+        spinner.start();
+
+        setTimeout(()=>{
+
+            spinner.stop();
+            console.log("Your order is successful \n")
+            cs.close();
+            return ;
+        },10000)
+
+
+
+    })
+}
 
 // we will be building  a  coffee shop --->
 
@@ -40,10 +70,12 @@ function Coffeeshop(){
         console.log("You selected",selectedCoffee.name);
 
         // You have to ask for payment  ,ask for 10 digits card
+        CardDetails(cs);
+      
         // You have to just print out loading -setTimeout
         // --->Out put your order is successful
 
-        cs.close();
+   
 
 
 
