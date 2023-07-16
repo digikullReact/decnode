e1cf5fac15df2f60cb21fbc03514f2eb0963f1be
shubh <shubhamdixit863@gmail.com>
2023-07-16 20:08:24 +0530

    Post request body

--- express/main.js
+++ express/main.js
@@ -1,12 +1,30 @@
 const express = require("express");  // we are requirung express module
 const app = express();  // We are calling express method to create an app object
 
+
+
+// How to get 
+// route params
+// query params
+
+app.use(express.json());// its a middleware that parses the json body
+
 app.get("/", (req, res) => {
     res.send("hi there");
 
 })
 
+
+app.get("/data", (req, res) => {
+    res.send("hi data");
+
+})
+
+// Getting request body
 app.post("/data",(req,res)=>{
+
+    console.log("Request body----",req.body);
+
    /**
     * res.setHeader("content-type","application/json");
     * res.write('{name:"shubham"}')
@@ -49,6 +67,22 @@ app.put("/data",(req,res)=>{
  
  })
 
+ //query Params
+app.get("/query", (req, res) => {
+    console.log(req.query);
+    res.send("hi there");
+
+})
+
+
+// route params
+ app.get("/:id", (req, res) => {
+    console.log(req.params.id);
+    res.send("hi there");
+
+})
+
+
  
 
 
@@ -59,3 +93,9 @@ app.listen("8080", () => {
 
 
 
+
+
+// You have to create a post route which sends the product inoformation
+//  /product -->POST route 
+// {name:"productname",brand:"",stock:""};
+// store it into file --->
